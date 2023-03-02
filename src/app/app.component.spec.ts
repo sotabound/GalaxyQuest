227be111014114b6025df742c4295cbbf64b568b
Scott Lassiter <slassiter@scholarshipamerica.org>
2023-03-02 14:01:32 -0600

    My first commit to the galaxy!

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'GalaxyQuest'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('GalaxyQuest');
+    expect(app.pageTitle).toEqual('GalaxyQuest');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'gq-root',
+  template:`
+  <div><h1>{{pageTitle}}</h1>
+    <div>Search the Stars!</div>
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'GalaxyQuest';
+  pageTitle: string = 'Galaxy Quest'
 }
